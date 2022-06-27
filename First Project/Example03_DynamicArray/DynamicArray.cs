fd9acbadd355359335d99b75bd17079ef8dc35f7
poongro <107188498+seeeeeobin@users.noreply.github.com>
2022-06-27 21:58:25 +0900

    C# - DataStructure
    - -

--- First Project/Example03_DynamicArray/DynamicArray.cs
+++ First Project/Example03_DynamicArray/DynamicArray.cs
@@ -12,8 +12,30 @@ namespace Example03_DynamicArray
         private const int DEFAULT_SIZE = 1;
         private int[] _data = new int[DEFAULT_SIZE];
 
-        public int Length;
-        public int Capacity;
+        public int this[int index]
+        {
+            get
+            {
+                return _data[index];
+            }
+
+            set
+            {
+                _data[index] = value;
+            }
+        }
+
+        public int Length; // 실제 데이터 개수
+       
+        // 프로퍼티 : 필드의 ㄱ밧을 쓰거나 읽을때 get 함수나 set 함수를 용이하게 만들어서 접근할 수 있는
+        //            get 접근자와 set 접근자를 구현할 수 있는 멤버
+        public int Capacity
+        {
+            get
+            {
+                return _data.Length;
+            }
+        }
 
         public void Add(int item)
         {
@@ -21,15 +43,40 @@ namespace Example03_DynamicArray
             if (Length >= Capacity)
             {
                 int[] tmp = new int[Capacity * 2];
-                tmp = _data;
                 for (int i = 0; i < Length; i++)
                 {
                     tmp[i] = _data[i];
                 }
                 _data = tmp;
             }
+            
             _data[Length] = item;
             Length++;
+        }
+
+        public bool Remove(int item)
+        {
+            bool isFounded = false;
+            for (int i = 0; i < Length; i++)
+            {
+                if (_data[i] == item)
+                {
+                    isFounded = true;
+                    RemoveAt(i);
+                    break;
+                }
+            }
+            return isFounded;
+        }
+
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Length - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Length - 1] = default(int);
+            Length--;
         }
     }
 }
